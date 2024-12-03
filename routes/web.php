4bef942d620521eef30f70012a884b6fead05e9b
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-12-03 16:38:04 +0200

    model, controller, routes, views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/post/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::view('/about', 'pages.about')->name('about');
